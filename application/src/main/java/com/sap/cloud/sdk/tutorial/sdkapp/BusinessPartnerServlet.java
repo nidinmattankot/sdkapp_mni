849de20d30fc5f8274b7372a41c5e52277edb935
Nidin Mattankot <nidin.mattankot@sap.com>
2019-11-21 17:01:42 +0530

    Code Complete

---
+++ application/src/main/java/com/sap/cloud/sdk/tutorial/sdkapp/BusinessPartnerServlet.java
@@ -0,0 +1,40 @@
+package com.sap.cloud.sdk.tutorial.sdkapp;
+
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
+
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
+
+@WebServlet("/api/business-partners")
+public class BusinessPartnerServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private static final Logger logger = CloudLoggerFactory.getLogger(BusinessPartnerServlet.class);
+
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            final List<BusinessPartner> businessPartners =
+                    new GetBusinessPartnersCommand().execute();
+
+            response.setContentType("application/json");
+            response.getWriter().write(new Gson().toJson(businessPartners));
+
+        } catch (final Exception e) {
+            logger.error(e.getMessage(), e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write(e.getMessage());
+        }
+    }
+}
